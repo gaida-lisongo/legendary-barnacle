31d275c125d67ee2e01b8d785574d21c8415fb9a
lisongonathan <lisongobaita@gmail.com>
2025-09-11 05:27:50 +0100

    fixe endpoints etudiants

--- routes/etudiant.js
+++ routes/etudiant.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const etudiantController = require('../controllers/etudiantController');
 
 // Etudiant routes
-router.post('/etudiant', etudiantController.createEtudiant);
-router.get('/etudiant', etudiantController.getEtudiants);
-router.get('/etudiant/:id', etudiantController.getEtudiant);
-router.put('/etudiant/:id', etudiantController.updateEtudiant);
-router.delete('/etudiant/:id', etudiantController.deleteEtudiant);
+router.post('/', etudiantController.createEtudiant);
+router.get('/', etudiantController.getEtudiants);
+router.get('/:id', etudiantController.getEtudiant);
+router.put('/:id', etudiantController.updateEtudiant);
+router.delete('/:id', etudiantController.deleteEtudiant);
 
 
 module.exports = router;
