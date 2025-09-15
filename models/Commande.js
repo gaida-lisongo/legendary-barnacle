e71254b8fd9eb644f160f8a861aba57d5733f436
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-09-15 10:26:47 +0000

    fix commande property

--- models/Commande.js
+++ models/Commande.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const CommandeSchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Produit', required: true },
-  statu: { type: String, enum: ['NO', 'PENDING', 'OK'], default: 'NO' },
+  status: { type: String, enum: ['NO', 'PENDING', 'OK'], default: 'NO' },
   reference: { type: String, required: true },
   anneeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Annee', required: true }
 });
