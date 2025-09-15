e5c5b0c887a65205f682cdc26e62ca230be33f5d
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-09-15 12:24:56 +0000

    add routes projets

--- routes/jury.js
+++ routes/jury.js
@@ -4,17 +4,17 @@ const juryController = require('../controllers/juryController');
 const juryClasseController = require('../controllers/juryController');
 
 // Jury CRUD
-router.post('/jury', juryController.createJury);
-router.get('/jury', juryController.getJuries);
-router.get('/jury/:id', juryController.getJury);
-router.put('/jury/:id', juryController.updateJury);
+router.post('/', juryController.createJury);
+router.get('/', juryController.getJuries);
+router.get('/:id', juryController.getJury);
+router.put('/id', juryController.updateJury);
 router.delete('/jury/:id', juryController.deleteJury);
 
 // JuryClasse CRUD
-router.post('/jury-classe', juryClasseController.createJuryClasse);
-router.get('/jury-classe', juryClasseController.getJuryClasses);
-router.get('/jury-classe/:id', juryClasseController.getJuryClasse);
-router.put('/jury-classe/:id', juryClasseController.updateJuryClasse);
-router.delete('/jury-classe/:id', juryClasseController.deleteJuryClasse);
+router.post('/classe', juryClasseController.createJuryClasse);
+router.get('/classe', juryClasseController.getJuryClasses);
+router.get('/classe/:id', juryClasseController.getJuryClasse);
+router.put('/classe/:id', juryClasseController.updateJuryClasse);
+router.delete('/classe/:id', juryClasseController.deleteJuryClasse);
 
 module.exports = router;
