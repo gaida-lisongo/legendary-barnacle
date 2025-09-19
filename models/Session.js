1f588b9951abfaaa93e7866d03c1d32d743d1a51
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-09-19 19:36:23 +0000

    fork session properties

--- models/Session.js
+++ models/Session.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const SessionSchema = new mongoose.Schema({
   anneeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Annee' },
   cours: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cours' }],
-  produitId: { type: mongoose.Schema.Types.ObjectId, ref: 'Produit' }
+  produitId: { type: mongoose.Schema.Types.ObjectId, ref: 'Produit' },
+  nomSession: { type: String, required: true },
+  dateDebut: { type: Date, required: true },
+  dateFin: { type: Date, required: true },
 });
 
 module.exports = mongoose.model('Session', SessionSchema);
